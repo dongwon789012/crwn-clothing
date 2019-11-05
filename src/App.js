affd95225fc3303e5de143366901ceda48e2e8ca
dongwon789012 <53262363+dongwon789012@users.noreply.github.com>
2019-11-05 18:27:51 +0900

    created project with create react app and added files for hompage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import HomePage from "./hompage.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomePage />
     </div>
   );
 }
+
+/*
+1.yarn add node-sass로 sass를 이용하자. create-react-app에는
+sass를 css로 변경해주는 babel webpack을 이미 가지고 있다. 버전 때문에 에러가 뜨면 following step을 따라하면 됨.
+나중에 문제 생기면 58강 체크
+
+2.
+
+
+*/
 
 export default App;
